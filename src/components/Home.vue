d3806a6d969e93c369c6bc08d0f87cd3dbb7a8ce
zMed-tech <badache.amn@gmail.com>
2021-10-01 17:22:57 +0100

    create home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <Map />
   </div>
 </template>
 
 <script>
+import Map from "@/components/Map.vue";
 export default {
   name: "Home",
+  components: { Map },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
